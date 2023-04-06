59d0964df0dda6926637403e5a1b15a86b40e0c6
Danilka312 <112857353+Danilka312@users.noreply.github.com>
2023-04-06 15:53:49 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,14 +4,18 @@ import numpy as np
 from scipy.stats import norm
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1119068275 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    alpha = 1 - p 
+    semians = 0 
+    for i in range(len(x)): 
+      semians += x[i] 
+    left = (semians - 0.065*len(x))/(len(x)*(alpha)) + 0.065
+    right = (semians - 0.065*len(x))/(len(x)*(1-alpha)) + 0.065
+
+    return left, \
+           right 
